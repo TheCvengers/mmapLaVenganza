a34cc00e5904740b12da9acdcc6dd18bddb83d35
Agustin DP <agus.diprinzio@gmail.com>
2015-06-12 19:05:43 -0300

    mmap andando piola

--- pruebaMmap/src/pruebaMmap.c
+++ pruebaMmap/src/pruebaMmap.c
@@ -17,81 +17,101 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <string.h>
-#define TAMANIO_BLOQUE (20 * 1024 * 1024)
+#define TAMANIO_BLOQUE 20*1024*1024
 
 
-int armarBloque(char *bloque_completo);
+int armarBloque(char *bloque_completo,int tamanio_aux);
+
+void armarBloqueFinal(char *bloque_final, int cantidad_escrita);
+
 
 int main(void) {
 
 	struct stat file;
-	int fd;
-	char *data;
-//	char *datos_a_guardar;
-//	char *datos_a_reciclar;
-	data = malloc (TAMANIO_BLOQUE);
+	int file_descriptor, tamanio_archivo;
+	int desplazamiento = 0;
+	int bytes_sobrantes = 0;
+	int tamanio_final = 0;
 
-	FILE *prueba_restantes = fopen ("pruebaRestantes.txt","w");
+	char *datos_a_copiar;
+	void *archivo_mapeado;
 
-	FILE *archivo_de_20 = fopen ("de20Megas.txt","w");
+	datos_a_copiar = malloc (TAMANIO_BLOQUE);
 
 	/*obtengo el descriptor del fichero*/
-	fd = open("/home/utnso/Escritorio/201301hourly.txt", O_RDONLY);
-	if(fd == -1){
+	file_descriptor = open("/home/utnso/Escritorio/201301hourly.txt", O_RDONLY);
+	if(file_descriptor == -1){
 		perror("Error al obtener el descriptor de fichero del archivo");
 		exit(1);
 	}
-	fflush(stdout);
 
-	fstat(fd, &file);
+	fstat(file_descriptor, &file);
+
+	tamanio_archivo = file.st_size;
+
+	archivo_mapeado = mmap((caddr_t)0, tamanio_archivo , PROT_READ, MAP_PRIVATE, file_descriptor, 0);
 
 
-	if((mmap((caddr_t)0, file.st_size, PROT_READ, MAP_SHARED, fd, 0)) == MAP_FAILED){
+	if( archivo_mapeado == MAP_FAILED){
 
 		perror("mmap");
 		exit(1);
 	}
-	else {
 
-		memcpy(data, mmap((caddr_t)0, TAMANIO_BLOQUE , PROT_READ, MAP_SHARED, fd, 0), TAMANIO_BLOQUE);
+	while (1){
+
+		if( (tamanio_archivo - desplazamiento) < TAMANIO_BLOQUE  ){
+
+			tamanio_final = (tamanio_archivo - desplazamiento);
+
+			memcpy(datos_a_copiar, archivo_mapeado + desplazamiento, tamanio_final); //cambio tamanio_bloque por tamanio_final
+
+			armarBloqueFinal(datos_a_copiar, tamanio_final );
+
+			free (datos_a_copiar);
+
+			break;
+
+		}
+
+		memcpy(datos_a_copiar , archivo_mapeado + desplazamiento, TAMANIO_BLOQUE);
+
+		bytes_sobrantes = armarBloque(datos_a_copiar, TAMANIO_BLOQUE);
+
+		desplazamiento = desplazamiento + (TAMANIO_BLOQUE - bytes_sobrantes);
+
 	}
 
-	fwrite (data, sizeof(char), TAMANIO_BLOQUE, archivo_de_20);
+	munmap(archivo_mapeado,file.st_size); //Buenas practicas segun foro
 
-	int sobran = armarBloque(data);
-
-	fwrite (data ,sizeof(char), TAMANIO_BLOQUE ,prueba_restantes);
-
-
-	printf("%d", sobran);
-	fflush(stdout);
-	//Buenas practicas segun foro
-	munmap(data,file.st_size);
-	close(fd);
-
-	free (data);
-
-	fclose(archivo_de_20);
-	fclose(prueba_restantes);
-	//free (datos_a_guardar);
-	//free (datos_a_reciclar);
+	close(file_descriptor);
 
 	return 0;
 }
 
-int armarBloque(char *bloque_completo){
+int armarBloque(char *bloque_completo , int tamanio_aux){
 
-	int restante = TAMANIO_BLOQUE - 1;
+	int restante = tamanio_aux - 1;
 
-	while(restante > 0 && bloque_completo[restante] != '\n')
+	while(restante >= 0 && bloque_completo[restante] != '\n')
 	{
-		if(bloque_completo[restante]== '\n')break;
-
-		bloque_completo[restante]= '0';
+		bloque_completo[restante] ='0';
 
 		restante--;
 
 	}
 
 	return TAMANIO_BLOQUE - (restante + 1);
+}
+
+void armarBloqueFinal(char *bloque_final, int cantidad_escrita){
+
+	int contador = TAMANIO_BLOQUE - 1;
+
+	while (contador >= cantidad_escrita ){
+
+		bloque_final[contador] = '0';
+
+		contador --;
+	}
 }
